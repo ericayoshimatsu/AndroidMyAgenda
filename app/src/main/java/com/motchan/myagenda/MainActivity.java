8459f4634ae97922d2480ae80f05e6aca252a8b7
Erica <ericayoshimatsu@gmail.com>
2019-09-08 21:38:38 +0200

    First Toast

--- app/src/main/java/com/motchan/myagenda/MainActivity.java
+++ app/src/main/java/com/motchan/myagenda/MainActivity.java
@@ -1,6 +1,16 @@
 package com.motchan.myagenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "HELLO WORLD!", Toast.LENGTH_LONG).show();
+    }
 }
