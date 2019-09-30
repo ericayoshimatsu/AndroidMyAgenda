9914e01d82fbef8242dace9089db7be01546a610
Erica <ericayoshimatsu@gmail.com>
2019-09-30 20:47:55 +0200

    App Bar and FAB icon

--- app/src/main/java/com/motchan/myagenda/MainActivity.java
+++ app/src/main/java/com/motchan/myagenda/MainActivity.java
@@ -8,23 +8,24 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "HELLO WORLD!", Toast.LENGTH_LONG).show();
         // activate layout
         setContentView(R.layout.activity_main);
-
+        setTitle("My List");
         // configure list
         List<String> alunos = new ArrayList<>(Arrays.asList("Ed","Edd","Eddy"));
         ListView listOfMain = findViewById(R.id.activity_main_list_view);
-        listOfMain.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
+        listOfMain.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 
 
     }
